f208a83a90189fcbe03b0501468ac6f04823d565
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-03-19 13:17:51 +0530

    fully set up def check_cllisions

--- main.py
+++ main.py
@@ -102,10 +102,19 @@ def change_direction(new_direction):
             direction = new_direction
 
 
-def check_collisions():
+def check_collisions(snake):
+    x, y = snake.coordinates[0]
 
-    pass
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
 
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
 
 
 def game_over():
