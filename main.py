03b58d95da090883360778b8f893f4b3c8be373a
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-03-19 13:04:37 +0530

    updated

--- main.py
+++ main.py
@@ -41,25 +41,38 @@ def next_turn(snake , food ):
    if direction == "up":
        y -=  SPACE_SIZE
    elif direction == "down":
-       y +=SPACE_SIZE
+       y += SPACE_SIZE
    elif direction == "left":
-       x -=SPACE_SIZE
+       x -= SPACE_SIZE
    elif direction == "right":
-       x +=SPACE_SIZE
+       x += SPACE_SIZE
 
-   snake.coordinates.insert(0 , (x,y))
+   snake.coordinates.insert(0, (x, y))
 
-   square = canvas .create_rectangle(x, y,x + SPACE_SIZE, y + SPACE_SIZE,fill=SNAKE_COLOR)
+   square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
 
    snake.squares.insert(0, square)
 
-   del snake.coordinates[-1]
+   if x == food.coordinates[0] and y == food.coordinates[1]:
 
-   canvas.delete(snake.squares[-1])
+       global score
 
-   del snake.squares[-1]
+       score += 1
 
-   window.after(SPEED ,next_turn,snake,food)
+       label.config(text="Score:{}".format(score))
+
+       canvas.delete("food")
+
+       food = Food()
+
+   else:
+       del snake.coordinates[-1]
+
+       canvas.delete(snake.squares[-1])
+
+       del snake.squares[-1]
+
+   window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
 
@@ -111,13 +124,13 @@ y = int((screen_height/2) -(window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-window.bind('<Left',lambda event:change_direction('left'))
-window.bind('<Right',lambda event:change_direction('right'))
-window.bind('<Up',lambda event:change_direction('up'))
-window.bind('<Down',lambda event:change_direction('down'))
+window.bind('<Left>',lambda event:change_direction('left'))
+window.bind('<Right>',lambda event:change_direction('right'))
+window.bind('<Up>',lambda event:change_direction('up'))
+window.bind('<Down>',lambda event:change_direction('down'))
 snake =Sanke()
 food = Food()
 
-next_turn(snake ,food)
+next_turn(snake, food)
 
 window.mainloop()
