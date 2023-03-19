9d8ef03de6adb835b27566c1488709d5f416c40f
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-03-19 11:56:26 +0530

    add snak  updated

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ BACKGROUND_COLOR = "#000000"
 
 
 class Sanke:
-    pass
 
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0 , BODY_PARTS):
+            self.coordinates.append([0 ,0])
+
+        for x, y in self.coordinates:
+            square =canvas.create_rectangle(x , y, x + SPACE_SIZE , y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 class Food:
     def __init__(self):
         x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
